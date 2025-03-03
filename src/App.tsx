b7f0b575fb7127e2070ef990ecb169982f46a45e
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2025-03-03 13:25:55 +0000

    Code added to change responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 import Dashboard from './pages/Dashboard';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
